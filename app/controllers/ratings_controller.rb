8679ac93bbfcdfefa73451db2f14fa8bf5156358
Erkki Heino <evsheino@gmail.com>
2013-09-15 17:26:55 +0300

    Rating delete

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,6 @@
 class RatingsController < ApplicationController
+  before_action :set_rating, only: [:destroy]
+
   def index
     @ratings = Rating.all
   end
@@ -13,7 +15,20 @@ class RatingsController < ApplicationController
     redirect_to ratings_path
   end
 
-  def rating_params
-    params.require(:rating).permit(:score, :beer_id)
+  def destroy
+    @rating.destroy
+    respond_to do |format|
+      format.html { redirect_to ratings_url }
+      format.json { head :no_content }
+    end
   end
+
+  private
+    def set_rating
+      @rating = Rating.find(params[:id])
+    end
+
+    def rating_params
+      params.require(:rating).permit(:score, :beer_id)
+    end
 end

f6fb42850d860c56bd44fa7bd17876c73f8ac41c
Erkki Heino <evsheino@gmail.com>
2013-09-19 17:21:58 +0300

    AverageRating module

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,11 +1,8 @@
 class Beer < ActiveRecord::Base
+  include RatingAverage
+
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
-
-  def average_rating
-    ratings.inject(0) {|sum, rating| sum + rating.score}.to_f / ratings.count
-    #ratings.average(:score)
-  end
 
   def to_s
     "#{brewery.name} #{name}"

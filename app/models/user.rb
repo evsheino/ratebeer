177a35208cb01fc0660dac5316f2497cd33c6081
Erkki Heino <evsheino@gmail.com>
2013-09-27 21:14:20 +0300

    Less raw SQL in favorite_beer

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,13 +21,10 @@ class User < ActiveRecord::Base
   # Get the style that has the highest average rating among beers the user has rated.
   def favorite_style
     return nil if ratings.empty?
-    qry = ActiveRecord::Base.connection.execute(
-        "SELECT style FROM beers b INNER JOIN ratings r ON b.id = r.beer_id
-        INNER JOIN users u ON u.id = r.user_id
-        GROUP BY u.id, style
-        HAVING u.id = #{id}
-        ORDER BY SUM(score)/COUNT(score) DESC LIMIT 1"
-    ).first['style']
+    Beer.joins(:raters).group('users.id', :style)
+      .having('users.id = ?', id)
+      .order('SUM(score)/COUNT(score) DESC')
+      .first[:style]
   end
 
   def to_s

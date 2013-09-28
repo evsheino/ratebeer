1a73f9dbf5f046222dc6ddcb516505da7d4c7ab1
Erkki Heino <evsheino@gmail.com>
2013-09-28 20:31:28 +0300

    User browser tests

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -7,33 +7,30 @@ describe "Breweries page" do
     expect(page).to have_content 'Number of breweries: 0'
   end
 
-  it "lists the existing breweries and their total number" do
-    breweries = ["Koff", "Karjala", "Schlenkerla"]
-    breweries.each do |brewery|
-      FactoryGirl.create(:brewery, :name => brewery)
+  describe "when breweries exists" do
+    before :each do
+      @breweries = ["Koff", "Karjala", "Schlenkerla"]
+      year = 1896
+      @breweries.each do |brewery|
+        FactoryGirl.create(:brewery, :name => brewery, :year => year += 1)
+      end
+
+      visit breweries_path
     end
 
-    visit breweries_path
-
-    expect(page).to have_content "Number of breweries: #{breweries.count}"
-
-    breweries.each do |brewery|
-      expect(page).to have_content brewery
-    end
-  end
-
-  it "allows the user to navigate to the page of a brewery" do
-    breweries = ["Koff", "Karjala", "Schlenkerla"]
-    year = 1896
-    breweries.each do |brewery|
-      FactoryGirl.create(:brewery, :name => brewery, :year => year += 1)
+    it "lists the breweries and their total number" do
+      expect(page).to have_content "Number of breweries: #{@breweries.count}"
+      @breweries.each do |brewery|
+        expect(page).to have_content brewery
+      end
     end
 
-    visit breweries_path
+    it "allows the user to navigate to the page of a brewery" do
+      click_link "Koff"
 
-    click_link "Koff"
+      expect(page).to have_content "Koff"
+      expect(page).to have_content "Established year 1897"
+    end
 
-    expect(page).to have_content "Koff"
-    expect(page).to have_content "Established year 1897"
   end
 end
---
+++ spec/features/users_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "User" do
+  before :each do
+    FactoryGirl.create :user
+  end
+
+  describe "who has signed up" do
+    it "can sign in with the right credentials" do
+      visit signin_path
+      fill_in('username', :with => 'Pekka')
+      fill_in('password', :with => 'foobar1')
+      click_button('Log in')
+
+      expect(page).to have_content 'Welcome back!'
+      expect(page).to have_content 'Pekka'
+    end
+
+    it "is redirected back to sign in form if wrong credentials given" do
+      visit signin_path
+      fill_in('username', :with => 'Pekka')
+      fill_in('password', :with => 'wrong')
+      click_button('Log in')
+
+      expect(current_path).to eq(signin_path)
+      expect(page).to have_content 'Username and password do not match!'
+    end
+  end
+
+  it "is added to the system after signing up with good credentials" do
+    visit signup_path
+    fill_in('user_username', :with => 'Brian')
+    fill_in('user_password', :with => 'secret55')
+    fill_in('user_password_confirmation', :with => 'secret55')
+
+    expect{
+      click_button('Create User')
+    }.to change{User.count}.by(1)
+  end
+end
